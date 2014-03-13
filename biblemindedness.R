b77b5475660d614a4b5be8ac4fad4289c5bf51dd
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2014-03-13 12:10:59 -0400

    Update biblemindedness.R

--- biblemindedness.R
+++ biblemindedness.R
@@ -1,12 +1,14 @@
 library(ggmap)
+library(stringr)
 
 usmap<-get_map('United States', zoom=4)
 
-parse.locations<-function(citystr) {
-  regex.results<-gregexpr('([A-Z])([a-z]+)(, [A-Z]{2})?', citystr)[[1]]
-  locations<-mapply(function(start, length) {
-    substr(citystr, start, start+length-1)
-  }, regex.results, attr(regex.results, 'match.length'))
+parse.locations<-function(citystr, sep='/') {
+  #regex.results<-gregexpr('([A-Z])([a-z]+)(, [A-Z]{2})?', citystr)[[1]]
+  #locations<-mapply(function(start, length) {
+  #  substr(citystr, start, start+length-1)
+  #}, regex.results, attr(regex.results, 'match.length'))
+  locations<-as.vector(mapply(str_trim, strsplit(citystr, sep)))
   statesuffix.results<-gregexpr(', [A-Z]{2}', locations)
   for (i in length(locations):1) {
     if (statesuffix.results[[i]][[1]]>0) {
